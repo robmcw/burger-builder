4453d47b9766a22b9ec389b47451a35178081715
Robert McWhirter <rob@robmcw.com>
2020-07-17 19:10:52 +0200

    Add functionality to add ingredients to burger dynamically using BurgerControls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import BurgerIngredient from "./components/Burger/BurgerIngredient/BurgerIngredient";
 
 class App extends Component {
   render() {
