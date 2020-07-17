7ef39eb6aff7c24b5aa110f86802197404708f7e
Robert McWhirter <rob@robmcw.com>
2020-07-17 20:51:55 +0200

    Allow remove ingredient, disable buttons when 0 ingredient, show price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,11 +11,17 @@ const controls = [
 
 const BuildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>
+      {" "}
+      Current price: <strong> ${props.totalPrice.toFixed(2)}</strong>
+    </p>
     {controls.map((ctrl) => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
       />
     ))}
   </div>
